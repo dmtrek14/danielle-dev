6725642817d688af132a367b967ee354b06396d3
Danielle <dmmayabb@gmail.com>
2020-08-17 15:29:31 -0700

    Styling updates
    Smoothing out styles for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/layout/seo"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faCodepen, faDev } from '@fortawesome/free-brands-svg-icons'
 
 export default (props) => {
   return (
@@ -11,10 +11,12 @@ export default (props) => {
       <h1>Danielle Mayabb</h1>
       <h2>full-stack developer | accessibility engineer | information architect</h2>
       <ul className="social">
-        <li> <FontAwesomeIcon icon={faGithub}/></li>
-        <li> <FontAwesomeIcon icon={faLinkedin}/></li>
+        <li><a href="https://github.com/dmtrek14"><FontAwesomeIcon icon={faGithub}/><span className="sr-only">Github profile</span></a></li>
+        <li><a href="https://www.linkedin.com/in/danielle-mayabb-4066a4105/"><FontAwesomeIcon icon={faLinkedin}/><span className="sr-only">LinkedIn profile</span></a></li>
+        <li><a href="https://codepen.io/dmtrek14"><FontAwesomeIcon icon={faCodepen}/><span className="sr-only">Codepen profile</span></a></li>
+        <li><a href="https://dev.to/thescifibrarian"><FontAwesomeIcon icon={faDev}/><span className="sr-only">Dev profile</span></a></li>
       </ul>
-      <p>Hello! I'm Danielle. I make things for the web.</p>
+      <p>Hello! I'm Danielle. I make things for the web (and beyond). I'm passionate about building accessible and well-designed applications. My combined background in the arts, information science, and engineering has helped me form an empathetic and user-focused approach to development.</p>
 
       <hr/>
       <div>
