48eaba4218e563a1d05b0db3c7132809ebdd7f23
Danielle <dmmayabb@gmail.com>
2020-08-18 20:02:25 -0700

    Small styling changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ export default (props) => {
   return (
     <Layout>
       <SEO title="Home" description="Welcome to my home page" pathname={props.location.pathname}/>
-      {/* <div></div> */}
       <div>
         <h1>Danielle Mayabb</h1>
         <h2>full-stack developer | accessibility engineer | information architect</h2>
@@ -19,11 +18,9 @@ export default (props) => {
           <li><a href="https://dev.to/thescifibrarian"><FontAwesomeIcon icon={faDev}/><span className="sr-only">Dev profile</span></a></li>
         </ul>
         <p>Hello! I'm Danielle. I make things for the web (and beyond). I'm passionate about building accessible and well-designed applications. My combined background in the arts, information science, and engineering has helped me form an empathetic and user-focused approach to development. I currently work for state government using a varied tech stack (mostly .NET, other bits-and-bobs), and I do hobby projects mostly in the JAMstack. </p>
-        
         <hr/>
         <h2>What I'm working on</h2>
       </div>
-      {/* <div></div> */}
     </Layout>
   )
 }
