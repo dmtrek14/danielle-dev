2ad433f031caab5a479214e5d1fa9d0da25d7d3a
Danielle <dmmayabb@gmail.com>
2020-09-06 18:17:07 -0700

    uninstall packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,9 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`);
-const { fmImagesToRelative } = require('gatsby-remark-relative-images');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
   if (node.internal.type === `Mdx`) {
-    fmImagesToRelative(node);
     const slug = createFilePath({ node, getNode});
     createNodeField({
       node, 
