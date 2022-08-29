da98bfc24f1c69d93a9d2ea96c538e7f503d5a07
Danielle Mayabb <dmmayabb@gmail.com>
2022-08-29 15:16:55 -0700

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import  { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
+import netlify from '@astrojs/netlify/functions';
 
 
 export default defineConfig ({
   site: 'https://danielle-m.dev',         
   integrations: [preact(), image()],
+  output: 'server',
+  adapter: netlify()
 });
