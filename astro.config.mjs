6207ad3c7b7e6ff5cacaf92f4b7689de386670b5
Danielle Mayabb <dmmayabb@gmail.com>
2022-08-29 16:17:54 -0700

    Adjust config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 import  { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import image from '@astrojs/image';
-import netlify from '@astrojs/netlify/functions';
+//import netlify from '@astrojs/netlify/functions';
 import scss from 'rollup-plugin-scss';
 
 
 export default defineConfig ({
   site: 'https://danielle-m.dev',         
   integrations: [preact(), image()],
-  output: 'server',
-  adapter: netlify(),
+  // output: 'static',
+  // adapter: netlify(),
+  // // vite: {
+  // //   build: {
+  // //     rollupOptions: {
+  // //       plugins: [
+  // //         scss()
+  // //       ]
+  // //     }
+  // //   }
+  // // }, 
   rollupOptions: {
     plugins: [scss()]
   }
